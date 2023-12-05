bdc53ffccbf0d9c0190b75510f93425a9466337d
Olim4ik <oakhmadjonov.uz@gmail.com>
2023-12-05 15:56:16 +0500

    test 1

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int charC = 99;
+        int charH = 104;
+        int charA = 97;
+        int charR = 114;
+
+        System.out.print((char) charC);
+        System.out.print((char) charH);
+        System.out.print((char) charA);
+        System.out.print((char) charR);
     }
 }
